02b1766ee7cd050bea195d6d0aef87e7950c09a6
Domenico159 <domenico.zummo@gmail.com>
2021-05-06 15:41:29 +0200

    Add base file

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+     <h1>Header here</h1>
+  </header>
+</template>
+
+<script>
+export default {
+    name:'Header',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <main>
+
+      <section class="container">
+          <div class="row">
+          <div class="col-6 col-md-4 col-lg-3">
+              <Disk />
+          </div>
+      </div>
+      </section>
+
+  </main>
+
+</template>
+
+<script>
+import Disk from './Disk.vue';
+export default {
+    name:'Main',
+    components: {
+        Disk,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
